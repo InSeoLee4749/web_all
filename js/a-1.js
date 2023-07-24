eae2bd98554dbf19de90fcb90c2510528ca6ca19
InSeoLee4749 <in474900@gmail.com>
2023-07-24 15:38:06 +0900

    a-1최종 완성본

--- js/a-1.js
+++ js/a-1.js
@@ -14,25 +14,21 @@
 
 
 $(function(){
-    // slide 옆으로
     setInterval(function(){
-        
-        $(".ani").animate({'marginLeft' : -1200}, 400, function(){
-            $(this).find('div').eq(0).appendTo($(this))
-            $(this).css("margin-Left",0)
-    })
+        $(".swiper-wrapper").animate({"marginLeft" : -1200}, 400, function(){
+            $(this).find("div").eq(0).appendTo($(this))
+            $(this).css("margin-left",0)
+        })
     }, 3000)
 
-    // content tab 부분
-    $(".tab2 h2").click(function(){
-        $(this).parent("li").addClass('act').siblings().removeClass('act')
+    $("#tab2 h2").click(function(){
+        $(this).parent("li").addClass("act").siblings().removeClass("act")
     })
 
-    // pop
     $(".notice li").eq(0).click(function(){
-        $(".pop").addClass('show')
+        $(".pop").addClass("show")
     })
     $(".pop button").click(function(){
-        $(".pop").removeClass('show')
+        $(".pop").removeClass("show")
     })
 })
